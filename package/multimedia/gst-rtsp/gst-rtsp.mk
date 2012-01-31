504b567bfbabd4330b05048fcbf30b3379be8fde
Nomi Xie <cfanfrank@gmail.com>
2012-01-31 15:38:05 +0800

    ++

---
+++ package/multimedia/gst-rtsp/gst-rtsp.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# gst-rtsp
+#
+#############################################################
+GST_RTSP_VERSION = 0.10.8
+GST_RTSP_SOURCE = gst-rtsp-$(GST_RTSP_VERSION).tar.bz2
+GST_RTSP_SITE = http://people.freedesktop.org/~wtay/
+GST_RTSP_LIBTOOL_PATCH = NO
+GST_RTSP_INSTALL_STAGING = YES
+GST_RTSP_INSTALL_TARGET = YES
+
+GST_RTSP_DEPENDENCIES = gst-plugins-base gstreamer
+
+$(eval $(call AUTOTARGETS))
+
